b7219914a9578a158856e4560d43dd7bb826d4ca
jamesaud <jamaudre@indiana.edu>
2019-05-04 14:48:16 +0300

    parsing nnf working

--- logic_conversion.jl
+++ logic_conversion.jl
@@ -1,28 +1,44 @@
-function eliminate_implication(expression::Expr)
-    exp = expression.args
-    P, Q = exp[2], exp[3]
-    return :[or, [not, $P], $Q]
-end
+using Match
 
-function eliminate_double_implications(expression::Expr)
-    exp = expression.args
-    P, Q = exp[2], exp[3]
-    return :[and, [or, $P, [not, $Q]], [or, [not, $P], $Q]]
-end
 
-function demorgan_or(expression::Expr)
-    P, Q = expression.args[2].args[2].args
-    return :[and, [not, $P], [not, $Q]]
-end
-
-function demorgan_and(expression::Expr)
-    P, Q = expression.args[2].args[2].args
-    return :[or, [not, $P], [not, $Q]]
-end
-
-function convert(expression::Expr)
-    operator = expression[1]     # This should be implicaton
-    if operator == :implication
+# Converts simple expression to negation normal form
+function nnf(expression::Array)
+    return @match expression begin
+      [:implies, P, Q]                 => [:or, [:not, P], Q]
+      [:double_implies, P, Q]          => [:and, [:or, P, [:not, Q]], [:or, [:not, P], Q]]
+      [:not, [:or, [P, Q]]]            => [:or, [:not, P], [:not, Q]]
+      [:not, [:and, [P, Q]]]           => [:or, [:not, P], [:not, Q]]
+      [:not, [:not, P]]                => P
+      [:not, [:all, x, Px]]            => [:all, x, [:not, Px]]
+      [:not, [:exists, x, Px]]         => [:exists, x, [:not, Px]]
+      X                                => X
     end
-    return "test"
+end
+
+function convert_expression(expression::Array)
+    expand(expression) = @match expression begin
+        s::Symbol => return s
+        e::Array => return convert_expression(nnf(e))
+    end
+
+    # recur(expression) = @match expression begin
+    #     s::Symbol => return s
+    #     e::Array => return convert_expression(e)
+    # end
+
+    expression = map(expand, expression)
+    return expression
+end
+
+function test_my(expression)
+    answer = convert_expression(expression)
+    println(answer)
+end
+
+function expand_expression(expression)
+    expand(exp) = @match expression begin
+        s::Symbol => return s
+        e::Expr => return [expand_expression(exp) for exp in e.args]
+    end
+    return expand(expression)
 end
--- tests.jl
+++ tests.jl
@@ -1,8 +1,37 @@
 using Test
 include("logic_conversion.jl")
 
-P = :[and, x, y]
-Q = :[or, x, y]
-@test eliminate_implication(:[implies, $P, $Q]) == :[or, [not, $P], $Q]
-@test demorgan_or(:[not, [or, [$P, $Q]]]) == :[and, [not, $P], [not, $Q]]
-@test demorgan_and(:[not, [and, [$P, $Q]]]) == :[or, [not, $P], [not, $Q]]
+P = [:and, :x, :y]
+Q = [:or, :x, :y]
+
+x = :x
+Px = [:P, x]
+
+# Input
+implication = [:implies, P, Q]
+double_implication = [:double_implies, P, Q]
+demorgan_or = [:not, [:or, [P, Q]]]
+demogran_and = [:not, [:and, [P, Q]]]
+double_not = [:not, [:not, P]]
+not_all = [:not, [:all, x, Px]]
+not_exists = [:not, [:exists, x, Px]]
+
+# Output of negation normal form
+implication_nnf = [:or, [:not, P], Q]
+double_implication_nnf = [:and, [:or, P, [:not, Q]], [:or, [:not, P], Q]]
+demorgan_or_nnf = [:or, [:not, P], [:not, Q]]
+demogran_and_nnf = [:or, [:not, P], [:not, Q]]
+double_not_nnf = P
+not_all_nnf = [:all, x, [:not, Px]]
+not_exists_nnf = [:exists, x, [:not, Px]]
+
+@test nnf(implication) == implication_nnf
+@test nnf(double_implication) == double_implication_nnf
+@test nnf(demorgan_or) == demorgan_or_nnf
+@test nnf(demogran_and) == demogran_and_nnf
+@test nnf(double_not) == double_not_nnf
+@test nnf(not_all) == not_all_nnf
+@test nnf(not_exists) == not_exists_nnf
+
+@test convert_expression([:and, implication, double_not]) ==
+                         [:and, implication_nnf, double_not_nnf]
